30dedcbea3d7840cc7b67e982bb6d9cf26322362
Peter Vegh <peter.vegh@arconsis.com>
2016-06-03 15:42:05 +0200

    using tag2

--- brewtap.rb
+++ brewtap.rb
@@ -1,9 +1,9 @@
 class Brewtap < Formula
   desc "my DESC"
   homepage ""
-  url "https://github.com/pvegh/brewtap/archive/tag1.tar.gz"
-  version "1"
-  sha256 "dcc4a697da196518a1e59b71462239c57dd0144dc638c693057ab045e585e0e8"
+  url "https://github.com/pvegh/brewtap/archive/tag2.tar.gz"
+  version "2"
+  sha256 "c35e8afa57d9ddd2b04bf92cec47d8a1bf0f699d2073a8ce0a50c5430f02320c"
 
   def install
     bin.install "testecho.sh"
